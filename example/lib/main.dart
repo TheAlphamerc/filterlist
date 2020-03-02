38779a7ed76e496984e8e0ccb61c9ebad15ef09a
Sonu Sharma <sonu.sharma045@gmail.com>
2020-03-02 22:35:44 +0530

    update plugin description in pubspec

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(title: 'Filter list plugin demo'),
     );
   }
