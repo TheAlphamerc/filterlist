bf3141f269c18d8688e7c0602d1b42bada901368
Sonu Sharma <sonu.sharma045@gmail.com>
2020-03-02 19:05:49 +0530

    filter popup design completed

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -44,68 +45,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final List<String> allTextList = ["One", "Two", "Three"];
+   List<String> selsctedTextList = [];
+  void _incrementCounter() async {
+   var list = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          elevation: 0.0,
+          backgroundColor: Colors.transparent,
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(40))),
+            height: MediaQuery.of(context).size.height * .5,
+            width: MediaQuery.of(context).size.width,
+            child: FilterList(
+              allTextList: allTextList,
+              headlineText: "Select Location",
+              searchFieldHintText: "Search Here",
+              selectedTextList: selsctedTextList,
+            ),
+          ),
+        );
+      },
+    );
+    if(list != null){
+      selsctedTextList = List.from(list);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+          //  child: FilterList(),
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
--- lib/filter_list.dart
+++ lib/filter_list.dart
@@ -1,7 +1,300 @@
 library filter_list;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class FilterList extends StatefulWidget {
+  FilterList(
+      {Key key,
+      this.selectedTextList,
+      this.allTextList,
+      this.headlineText = "Select here",
+      this.searchFieldHintText = "Search here"})
+      : super(key: key);
+  final List<String> selectedTextList;
+  final List<String> allTextList;
+
+  final String headlineText;
+  final String searchFieldHintText;
+
+  @override
+  _FilterListState createState() => _FilterListState();
+}
+
+class _FilterListState extends State<FilterList> {
+  List<String> _selectedTextList = List();
+
+  List<String> _allTextList;
+
+  @override
+  void initState() {
+    _allTextList = List.from(widget.allTextList);
+    _selectedTextList = widget.selectedTextList != null
+        ? List.from(widget.selectedTextList)
+        : [];
+    super.initState();
+  }
+
+  bool showApplyButton = false;
+ 
+  Widget _body() {
+    return Container(
+      height: MediaQuery.of(context).size.height - 100,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 2.5),
+            child: Container(
+               decoration: BoxDecoration(
+              color: Colors.white,
+                boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              offset: Offset(0, 5),
+                              blurRadius: 15,
+                              color: Color(0x12000000),
+                            )
+                ]
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Expanded(
+                          flex: 1,
+                          child: Container(),
+                        ),
+                        Expanded(
+                            flex: 6,
+                            child: Center(
+                              child: Text(
+                                widget.headlineText.toUpperCase(),
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline
+                                    .copyWith(fontSize: 18),
+                              ),
+                            )),
+                        Expanded(
+                          flex: 1,
+                          child: InkWell(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            onTap: () {
+                              Navigator.pop(context, List<String>());
+                            },
+                            child: Container(
+                              height: 25,
+                              width: 25,
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black87),
+                                  shape: BoxShape.circle),
+                              child: Icon(Icons.close),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Material(
+                      // need Material wrapper for the TextField
+                      color: Colors
+                          .transparent, // get rid of Materials background color
+                      child: Container(
+                        margin: EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: Colors.grey.shade200),
+                        child: TextField(
+                          onChanged: (value) {
+                            // update list based on text
+                            setState(() {
+                              _allTextList = _allTextList
+                                  .where((string) => string
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()))
+                                  .toList();
+                            });
+                          },
+                          style: TextStyle(fontSize: 18, color: Colors.black87),
+                          decoration: InputDecoration(
+                            prefixIcon:
+                                Icon(Icons.search, color: Colors.black38),
+                            hintText: widget.searchFieldHintText,
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                   
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: Text(
+              '${_selectedTextList.length} selected items',
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ),
+          Expanded(
+              child: Container(
+            padding: EdgeInsets.only(top: 0, bottom: 60, left: 5, right: 5),
+            child: SingleChildScrollView(
+              child: Wrap(
+                children: _buildChoiceList(_allTextList),
+              ),
+            ),
+          )),
+           _controlButon() 
+        ],
+      ),
+    );
+  }
+
+  Widget _controlButon() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 40,
+        width: MediaQuery.of(context).size.width * .9,
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        alignment: Alignment.center,
+        child: Row(
+          children: <Widget>[
+            Expanded(child: SizedBox()),
+            Container(
+              //  borderRadiusValue: 40,
+             
+              child: Row(
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _selectedTextList = List.from(_allTextList);
+                      });
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      child: Text(
+                        'All',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline
+                            .copyWith(fontSize: 20, color: Colors.blue),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        _selectedTextList.clear();
+                      });
+                    },
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      child: Text(
+                        'Reset',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline
+                            .copyWith(fontSize: 20, color: Colors.blue),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  MaterialButton(
+                      color: Colors.blue,
+                      padding: EdgeInsets.only(bottom: 5),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      onPressed: () {
+                        Navigator.pop(context, _selectedTextList);
+                      },
+                      child: Center(
+                        child: Text(
+                          'Apply',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline
+                              .copyWith(fontSize: 20, color: Colors.white),
+                          textAlign: TextAlign.center,
+                        ),
+                      ))
+                ],
+              ),
+            ),
+            Expanded(child: SizedBox()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(bottom: 3, top: 3),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.white),
+        child: Stack(
+          children: <Widget>[
+            _body(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildChoiceList(List<String> list) {
+    List<Widget> choices = List();
+    list.forEach((item) {
+      choices.add(Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: ChoiceChip(
+          backgroundColor: Colors.grey.shade100,
+          selectedColor: Colors.blue,
+          label: _selectedTextList.contains(item)
+              ? Text(
+                  '$item',
+                  style: TextStyle(color: Colors.white),
+                )
+              : Text(
+                  '$item',
+                ),
+          selected: _selectedTextList.contains(item),
+          onSelected: (selected) {
+            setState(() {
+              _selectedTextList.contains(item)
+                  ? _selectedTextList.remove(item)
+                  : _selectedTextList.add(item);
+              onSelectionChanged(_selectedTextList);
+            });
+          },
+        ),
+      ));
+    });
+    var wrap = list == null || list.length == 0
+        ? Container()
+        : Wrap(
+            children: choices,
+          );
+
+    return choices;
+  }
+
+  onSelectionChanged(List<String> list) {}
 }
