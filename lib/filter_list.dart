080e386c81938fb77309ace7ac44270407de202a
Sonu Sharma <sonu.sharma045@gmail.com>
2022-01-18 17:09:44 +0530

    📸  Added screenshots

--- lib/filter_list.dart
+++
@@ -1,5 +0,0 @@
-library filter_list;
-
-export 'src/filter_list_dialog.dart';
-export 'src/filter_list_delegate.dart';
-export 'src/theme/theme.dart';
--- lib/src/theme/theme.dart
+++
@@ -1,6 +0,0 @@
-export 'filter_list_theme.dart';
-export 'choice_chip_theme.dart';
-export 'header_theme.dart';
-export 'contol_button_theme.dart';
-export 'control_button_bar_theme.dart';
-export 'filter_list_delegate_theme.dart';
